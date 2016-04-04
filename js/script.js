45e04ca7db9a50d293d914735ac2d26307112c63
Juston Miller <geekwise.juston.miller@gmail.com>
2016-04-04 21:57:17 +0000

    new update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-var random_number = function()
+// var random_number = function()
 
 
 var html_element;
 
 var make_element = function(element_name){
         var element = document.createElement(element_name);
-        element.textContent = random_number();
+        element.textContent = element_name;
         document.body.appendChild(element);
         
         
@@ -13,30 +13,28 @@ var make_element = function(element_name){
         
 }
 
+// var  ul;
 
+// var  li;
 
+// ul = document.createElement('ul');
 
-
-var  ul;
-
-var  li;
-
-ul = document.createElement('ul');
-
-li = document.createElement('li');
+// li = document.createElement('li');
 
 
 
 document.addEventListener('DOMContentLoaded',function(){
     
+        make_element('ul');    
     
-        document.body.appendChild(ul);
-        document.body.appendChild(li);
+    
+//         document.body.appendChild(ul);
+//         document.body.appendChild(li);
 
 
-    ul.textContent = 'my ul tag';
+//     ul.textContent = 'my ul tag';
 
-    li.textContent = 'my li tag';
+//     li.textContent = 'my li tag';
     
 });
 
