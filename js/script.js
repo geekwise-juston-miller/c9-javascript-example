32130f6acb9c128ad376ec317682e03d98a7eff5
Juston Miller <geekwise.juston.miller@gmail.com>
2016-04-06 22:27:43 +0000

    img fix

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var add_images = function(number_of_images){
     var image;
     
     for(var i=0; i< number_of_images; i++){
-        img_urls.push('/images/profile_photo_' + (i + 1) + '.jpg');
+        img_urls.push('images/profile_photo_' + (i + 1) + '.jpg');
         image = document.createElement('img');
         image.setAttribute('src', img_urls[i]);
         document.body.appendChild(image);
