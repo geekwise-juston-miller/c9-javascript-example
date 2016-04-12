637ec36e52d481cb8ce3e173c89ca99d4568d9a3
Juston Miller <geekwise.juston.miller@gmail.com>
2016-04-12 23:40:53 +0000

    javascrit emojoi's work

--- js/script.js
+++ js/script.js
@@ -1,65 +1,26 @@
-var photo_container;
+var http_request = new XMLHttpRequest;
+http_request.open('GET','https://api.github.com', false);
+http_request.send (null);
 
-var img_urls = new Array;
+var github_data = JSON.parse(http_request.response);
 
-var add_images = function(number_of_images){
+http_request = new XMLHttpRequest;
+http_request.open('GET',github_data.emojis_url,false);
+http_request.send(null);
+
+var github_emjois = JSON.parse(http_request.response);
+
+
+document.addEventListener('DOMContentLoaded',function(event){
+    for(var i in github_emjois){
     
-    var image;
-    
-    
-    for(var i=0; i< number_of_images; i++){
-        img_urls.push('images/profile_photo_' + (i + 1) + '.jpg');
-        image = document.createElement('img');
-        image.setAttribute('src', img_urls[i]);
-        image.style.width = number_of_images;
-        document.body.appendChild(image);
-        
-        
-         image.addEventListener('mouseover', function(event){
-             
-             this.style.transform = 'rotateX(360deg)';
-             this.style.width = '250px';
-             this.style.height = '250px';
-             this.style.transition = 'width 1s, height 1s, transform 1s';
-         });
-         
-         image.addEventListener('mouseout', function(event){
-             
-             this.style.transform = 'rotateX(0deg)';
-             this.style.width = '75px';
-             this.style.height = '75px';
-             this.style.transition = 'width .5s, height .5s, transform .5s';
-        
-        
-         });
-         
-        //  image.addEventListener('click', function(event) {
-             
-        //      this.style.transform = "rotateY(180deg)";
-             
-             
-        //  });
-         
-         
-    }
-};
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function(event){
-    
-    add_images(68,'75px');
-    
-    
+    var emojoi_imgs = document.createElement('img');
+    emojoi_imgs.setAttribute('src',github_emjois[i]);
+    document.body.appendChild(emojoi_imgs);
    
+};
+    
+    
+     
     
 });
-
-
-
-
-
-
-
